5bc0fb21ff96ba5ff9f656f2ad4b50a1f8c2928e
Yuya <sato.yuya1211@gmail.com>
2018-05-27 04:11:41 +0000

    trendsコントローラーの追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
+  get 'trends/index'
+
   get '/' => 'home#top'
   get 'about' => 'home#top'
-  get 'trends/index' => 'home#top'
+  get 'trends/index' => 'trends#index'
 end
