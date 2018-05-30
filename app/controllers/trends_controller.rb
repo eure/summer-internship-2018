1ec6cf16010bc12ac7ea7803b9b3efeb194692d4
Yuya <sato.yuya1211@gmail.com>
2018-05-30 16:28:39 +0000

    githubトレンドリストをビューにて表示する

--- app/controllers/trends_controller.rb
+++ app/controllers/trends_controller.rb
@@ -1,4 +1,10 @@
 class TrendsController < ApplicationController
+  # lib/scrape/github.rbを読み込み
+  require 'scrape/github'
+  # Githubの情報をscrapeするモジュールを読み込む
+  include GithubScrape
   def index
+    # Github Trendingから情報を取得
+    @trends = GithubScrape.getTrends
   end
 end
