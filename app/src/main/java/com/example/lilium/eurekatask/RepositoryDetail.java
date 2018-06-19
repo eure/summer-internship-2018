8cad429d467313696e26106d08333aad10ba1187
lilium <lim.intefx@gmail.com>
2018-06-20 02:28:19 +0900

    this is first and last commit

---
+++ app/src/main/java/com/example/lilium/eurekatask/RepositoryDetail.java
@@ -0,0 +1,13 @@
+package com.example.lilium.eurekatask;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class RepositoryDetail extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_repository_detail);
+    }
+}
