e8bf4f2effe9e803de71dcf307bf4253319c36c6
ttexan <ttexan.developper@gmail.com>
2018-06-28 11:46:50 +0900

    fix error

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
   root 'articles#index'
-  get 'articles/index'
-  get 'articles/show'
+  resources :articles
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
