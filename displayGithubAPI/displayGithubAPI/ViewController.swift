442621cea249604fd0607de473b554b0b285d5f1
濱口和希 <>
2018-07-01 12:40:16 +0900

    [add]gitignore

---
+++ displayGithubAPI/displayGithubAPI/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  displayGithubAPI
+//
+//  Created by 濱口和希 on 2018/06/30.
+//  Copyright © 2018年 HamaguchiKazuki. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
