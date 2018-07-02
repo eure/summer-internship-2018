9c866b21e223330c7c4be444e4f61f6559edbc46
Atsushi KONISHI <crossover.ak@outlook.jp>
2018-07-03 02:34:19 +0900

    removed reset.css at detail.ejs

--- app.js
+++ app.js
@@ -3,10 +3,10 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var scraping = require('./scraping');
 
 var indexRouter = require('./routes/index');
 
-var scraping = require('./scraping');
 // なぜか動かない
 // var detailRouter = require('./routes/detail');
 
