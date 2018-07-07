f5a3f961c83e1d043f69cbf9fe7a9723233dbf0d
KO-YAMAKO <ko.foot1103@gmail.com>
2018-07-07 14:32:38 +0900

    pull force from upstream master

---
+++ Pods/Target Support Files/Pods-githubAPISampleTests/Pods-githubAPISampleTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_githubAPISampleTests : NSObject
+@end
+@implementation PodsDummy_Pods_githubAPISampleTests
+@end
---
+++ Pods/Target Support Files/Pods-githubAPISampleUITests/Pods-githubAPISampleUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_githubAPISampleUITests : NSObject
+@end
+@implementation PodsDummy_Pods_githubAPISampleUITests
+@end
