6dc133a5ad67812fc931426df4dbaa6aca871ee1
松平礼史 <matsudaira@matsudairareijinoMacBook-puro.local>
2018-07-10 04:06:43 +0900

    init

---
+++ summer-internship-2018/summer-internship-2018Tests/summer_internship_2018Tests.swift
@@ -0,0 +1,36 @@
+//
+//  summer_internship_2018Tests.swift
+//  summer-internship-2018Tests
+//
+//  Created by 松平礼史 on 2018/07/10.
+//  Copyright © 2018年 松平礼史. All rights reserved.
+//
+
+import XCTest
+@testable import summer_internship_2018
+
+class summer_internship_2018Tests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
