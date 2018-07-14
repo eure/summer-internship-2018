2461065f9d17711be1dbb3c77ec8cfe46a8b3dd1
fumiya37 <fumiya.t.bb7@gmail.com>
2018-07-15 00:29:11 +0900

    ファイル上げ直し

--- Github-ios-app/Github-ios-app/DetailViewController.swift
+++ Github-ios-app/Github-ios-app/DetailViewController.swift
@@ -1,17 +1,15 @@
 import UIKit
-import WebKit
-import Foundation
 import Alamofire
 import SwiftyJSON
+import Foundation
 
-class WebViewController: UIViewController, WKNavigationDelegate{
+class DetailViewController: UIViewController{
     
     @IBOutlet weak var repositoryName: UILabel!
     @IBOutlet weak var userName: UILabel!
     @IBOutlet weak var createdDate: UILabel!
     @IBOutlet weak var language: UILabel!
     @IBOutlet weak var repositorySize: UILabel!
-    
     var url:String!
     
     override func viewDidLoad() {
@@ -19,30 +17,33 @@ class WebViewController: UIViewController, WKNavigationDelegate{
         getRepository()
     }
     
+    //リポジトリの情報を取得する
     func getRepository() {
         Alamofire.request(url)
             .responseJSON{ response in
                 guard let object = response.result.value else {
                     return
                 }
-                let json = JSON(object)
-                print(json)
-                self.repositoryName.text = json["full_name"].string
-                self.userName.text = json["name"].string
-                self.createdDate.text = json["created_at"].string
-                self.language.text = json["language"].string
-                self.repositorySize.text = (json["size"].int)?.description
+                let statuscode = response.response?.statusCode
+                if(statuscode == 200){
+                    let json = JSON(object)
+                    //Labelに取得した情報を表示
+                    self.repositoryName.text = json["full_name"].string
+                    self.userName.text = json["name"].string
+                    self.createdDate.text = ((json["created_at"].string)?.replacingOccurrences(of: "T" , with: " "))?.replacingOccurrences(of: "Z", with: "")
+                    self.language.text = json["language"].string
+                    self.repositorySize.text = (json["size"].int)?.description
+                }else{
+                    self.repositoryName.text = "データの取得に失敗しました"
+                }
         }
     }
-    
+    //一つ前（ViewController）への画面遷移
     @IBAction func backNavigationButton(_ sender: Any) {
         self.dismiss(animated: true, completion: nil)
     }
-    
-    
+        
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
     }
-    
-    
 }
--- Github-ios-app/Github-ios-app/ViewController.swift
+++ Github-ios-app/Github-ios-app/ViewController.swift
@@ -1,25 +1,77 @@
-//
-//  ViewController.swift
-//  Github-ios-app
-//
-//  Created by Fumiya Takahashi on 2018/07/04.
-//  Copyright © 2018年 Fumiya Takahashi. All rights reserved.
-//
-
 import UIKit
+import Alamofire
+import SwiftyJSON
+import Foundation
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate{
+    
+    @IBOutlet weak var eventTableView: UITableView!
+    var Repositorys: [[String: String?]] = []
+    var url:String!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        getRepositoryEvent()
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return Repositorys.count
+    }
+    
+    //eventTableViewに取得した情報を表示
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell(style: .subtitle, reuseIdentifier: "cell")
+        let Repository = Repositorys[indexPath.row]
+        cell.textLabel?.text = Repository["repository"]!
+        cell.detailTextLabel?.text = Repository["event"]!
+        return cell
+    }
+    
+    //公開リポジトリのイベント情報を取得する
+    func getRepositoryEvent() {
+        Repositorys.removeAll()
+        Alamofire.request("https://api.github.com/events")
+            .responseJSON{ response in
+                guard let object = response.result.value else {
+                    return
+                }
+                let statuscode = response.response?.statusCode
+                if(statuscode == 200){
+                    let json = JSON(object)
+                    json.forEach{(_,json) in
+                        let Repository: [String: String?] = [
+                        "repository": json["repo"]["name"].string,
+                        "event": json["type"].string,
+                        "url": json["repo"]["url"].string
+                    ]
+                    self.Repositorys.append(Repository)
+                    }
+                }else{
+                    self.Repositorys.append(["repository":"データの取得に失敗しました"])
+                }
+                self.eventTableView.reloadData()
+        }
+    }
+    
+    //eventTableViewの中身を最新のものにする
+    @IBAction func refreshNavigationButton(_ sender: Any) {
+        getRepositoryEvent()
+    }
+    
+    //DetailViewControllerへの画面遷移
+    func tableView(_ table: UITableView,didSelectRowAt indexPath: IndexPath) {
+        let Repository = Repositorys[indexPath.row]
+        url = Repository["url"]!
+        performSegue(withIdentifier: "toWebViewController",sender: nil)
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any!) {
+        let DetailVC: DetailViewController = (segue.destination as? DetailViewController)!
+        DetailVC.url = url
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
-
-
 }
 
