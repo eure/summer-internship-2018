d759933853629b52d99843a429a605a0179cbe71
gsittyz <27331655+gsittyz@users.noreply.github.com>
2018-07-16 22:01:41 -0700

    move app.use(logger()) just after requiring koa-logger

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
 const Koa = require('koa');
 const app = new Koa();
 const logger = require('koa-logger')
+app.use(logger());
 
 app.use(async ctx => {
   ctx.body = 'Hello World';
 });
 
-app.use(logger());
+
 app.listen(3000);
 
