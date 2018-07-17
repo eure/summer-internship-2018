240f9a408f56e47c5748183fad68a5e8befb2376
gsittyz <27331655+gsittyz@users.noreply.github.com>
2018-07-16 22:30:31 -0700

    added koa-logger, koa-router

--- app.js
+++ app.js
@@ -1,12 +1,23 @@
 const Koa = require('koa');
 const app = new Koa();
 const logger = require('koa-logger')
+const Pug = require('koa-pug')
 app.use(logger());
 
-app.use(async ctx => {
-  ctx.body = 'Hello World';
+const router = require('koa-router')();
+
+app.use(router.routes());
+
+router.get('/', async (ctx, next) => {
+  ctx.render('index');
 });
 
 
-app.listen(3000);
+const pug = new Pug({
+  viewPath: './views',
+  basedir: './views',
+  app: app // equals to pug.use(app) and app.use(pug.middleware)
+});
 
+
+app.listen( process.env.PORT || 3000);
