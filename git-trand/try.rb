f91694b3b2f672d99ab80d3498b1ae0a0b744b16
EC2 Default User <ec2-user@ip-172-31-30-39.us-east-2.compute.internal>
2018-07-22 09:02:48 +0000

    create try.eb

---
+++ git-trand/try.rb
@@ -0,0 +1,31 @@
+require 'nokogiri'
+require 'open-uri'
+
+
+
+
+url = 'https://github.com/trending'
+
+charset = nil
+html = open(url) do |f|
+  charset = f.charset 
+  f.read 
+end
+
+
+doc = Nokogiri::HTML.parse(html, nil, charset)
+titles = []
+contents = []
+doc.xpath('//li[@class="col-12 d-block width-full py-4 border-bottom"]').each do |node|
+   title =  node.xpath('.//h3/a/@href')[0].text
+   title.slice!(0)
+   titles.push(title)
+   
+
+   
+   content = node.xpath('.//p[@class="col-9 d-inline-block text-gray m-0 pr-4"]').text
+   content.slice!("\n")
+   content.strip!
+   contents.push(content)
+  
+end
