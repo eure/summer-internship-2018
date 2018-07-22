103a5dc34665bee13fb4ed25c8dae16c9ba97b86
NoritomoTaba <minamiedamame0328@gmail.com>
2018-07-23 08:14:07 +0900

    added routing

--- trend_view/lib/TrendView.pm
+++ trend_view/lib/TrendView.pm
@@ -5,19 +5,25 @@ use Mojolicious::Plugin::CSRFDefender;
 
 # This method will run once at server start
 sub startup {
-  my $self = shift;
+	my $self = shift;
 
-  # Load configuration from hash returned by "my_app.conf"
-  my $config = $self->plugin('Config' => +{file => 'etc/trend_view.conf'});
+	# Load configuration from hash returned by "my_app.conf"
+	my $config = $self->plugin('Config' => +{file => 'etc/trend_view.conf'});
 
-  # For CSRF
-  $self->plugin('Mojolicious::Plugin::CSRFDefender');
+	# For CSRF
+	$self->plugin('Mojolicious::Plugin::CSRFDefender');
 
-  # Router
-  my $r = $self->routes;
+	# Router
+	my $r = $self->routes;
 
-  # Normal route to controlloer
-  $r->get('/')->to('Ctrl_app#title');
+	# タイトルへのルーティング
+	$r->get('/')->to('Ctrl_app#title');
+
+	# リポジトリのトレンド一覧へのルーティング
+	$r->get('/trending')->to('Ctrl_app#repository');
+
+	# デベロッパーのトレンド一覧へのルーティング
+	$r->get('trending/developers')->to('Ctrl_app#developers');
 }
 
 1;
