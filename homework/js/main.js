9278ebd713663bc67699f4e4bf0e09de98d0c215
hidayoshi <toto0818summer@gaail.com>
2018-07-25 13:16:27 +0900

    とりあえず完成

--- homework/js/main.js
+++ homework/js/main.js
@@ -1,46 +1,28 @@
-    var vm = new Vue({
-        el: '#app',
-        data: {
-            github: {
-                repos:[
-                    open=false,
-                    change: function (repo) {
-                if (repo.open === false) {
-                    repo.open = true;
-                } else {
-                    repo.open = false;
-                }
-                console.log(repo.open);
-            }
-                ]
-            },
-            user: null,
-            isVisible: false
-        },
-        methods: {
-            getAPI: function () {
-                let user = this.user;
-                $.get('https://api.github.com/users/' + user + '/repos').then((repos) => {
-                    vm.github.repos = repos;
-                    console.log(repos);
-                });
-            },
-
-        }
-    });
-        //        change: function(index) {
-        //            if(this.github[index].open === false) this.github[index].open = true;
-        //            else this.github[index].open = false;
-        //            console.log(this.github[index].open);
-        //        }
-
-
-
-//   change: function () {
-//                if (this.github.repos.open === false) {
-//                    this.github.repos.open = true;
-//                } else {
-//                    this.github.repos.open = false;
-//                }
-//                console.log(this.github.repos.open);
-//            }
+var vm = new Vue({
+  el: "#app",
+  data: {
+    github: {
+      repos: []
+    },
+    user: null,
+    isVisible: false
+  },
+  methods: {
+    getAPI: function() {
+      let user = this.user;
+      $.get("https://api.github.com/users/" + user + "/repos").then(repos => {
+        vm.github.repos = repos;
+        // this.addDetailBotton();
+        // console.log(repos);
+      });
+    }
+    //  addDetailBotton: function () {
+    //     for (let i = 0; i < vm.github.repos.length; i++) {
+    //         vm.github.repos[i].open = false;
+    //         vm.github.repos[i].index = i;
+    //         console.log(vm.github.repos[i].open);
+    //         console.log(vm.github.repos[i].index);
+    //     }
+    // }
+  }
+});
