6b696579ba9c619d17b444fe107719e38fe61c28
hidayoshi <toto0818summer@gaail.com>
2018-07-25 14:42:08 +0900

    仮完成

--- homework/js/main.js
+++ homework/js/main.js
@@ -12,17 +12,7 @@ var vm = new Vue({
       let user = this.user;
       $.get("https://api.github.com/users/" + user + "/repos").then(repos => {
         vm.github.repos = repos;
-        // this.addDetailBotton();
-        // console.log(repos);
       });
     }
-    //  addDetailBotton: function () {
-    //     for (let i = 0; i < vm.github.repos.length; i++) {
-    //         vm.github.repos[i].open = false;
-    //         vm.github.repos[i].index = i;
-    //         console.log(vm.github.repos[i].open);
-    //         console.log(vm.github.repos[i].index);
-    //     }
-    // }
   }
 });
