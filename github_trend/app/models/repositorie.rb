ed7f922d90de23923409bbe91aabcb4e52d7473f
k-nasa <htilcs1115@gmail.com>
2018-07-29 20:44:44 +0900

    add/ 詳細画面のためにreadmeデータを取得する

--- github_trend/app/models/repositorie.rb
+++ github_trend/app/models/repositorie.rb
@@ -3,4 +3,15 @@ class Repositorie < ApplicationRecord
   validates :type, presence: true
   validates :name, length: { maximum: 25 }
   validates :discription, length: { maximum: 255 }
+
+  after_create :fetch_detail_data
+
+  def fetch_detail_data
+    html = open("https://github.com/#{name}")
+
+    doc = Nokogiri::HTML.parse(html)
+    readme = doc.css("#readme").to_s
+
+    update!(readme: readme)
+  end
 end
