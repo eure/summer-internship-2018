f922434fb772a38124460c8bedcbd258662ce458
culumn <yoxtsushi.44106@gmail.com>
2018-08-01 00:18:53 +0900

    add api client

---
+++ GitignoreTemplates/APIClientImpl.swift
@@ -0,0 +1,45 @@
+//
+//  APIClientImpl.swift
+//  GitignoreTemplates
+//
+//  Created by Matsuoka Yoshiteru on 2018/08/01.
+//  Copyright © 2018年 culumn. All rights reserved.
+//
+
+import Foundation
+
+final class APIClientImpl {
+
+    static let shared = APIClientImpl()
+
+    enum Path {
+        case templateList
+        case template
+
+        var baseURL: String {
+            return "https://api.github.com"
+        }
+
+        var endpoint: String {
+            switch self {
+            case .templateList, .template:
+                return baseURL + "/gitignore/templates"
+            }
+        }
+    }
+}
+
+// MARK: - APIClient
+extension APIClientImpl: APIClient {
+
+    func fetchAvailableTemplateList(completionHandler: @escaping (Data?, URLResponse?, Error?) -> Void) {
+        guard let url = URL(string: Path.templateList.endpoint) else { return }
+        URLSession.shared.dataTask(with: url, completionHandler: completionHandler).resume()
+    }
+
+    func fetchTemplateSource(of name: String,
+                             completionHandler: @escaping (Data?, URLResponse?, Error?) -> Void) {
+        guard let url = URL(string: Path.template.endpoint + "/" + name) else { return }
+        URLSession.shared.dataTask(with: url, completionHandler: completionHandler).resume()
+    }
+}
