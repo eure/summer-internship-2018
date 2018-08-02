b83db6d0e9a57769d61dacc7903e8d3b01511ea6
Shawn Rivers <kaushou@gmail.com>
2018-08-02 11:23:39 +0900

    Add top bars

---
+++ src/Components/NavBarDetails.js
@@ -0,0 +1,30 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import IconButton from '@material-ui/core/IconButton';
+import Icon from '@material-ui/core/Icon';
+import Typography from '@material-ui/core/Typography';
+
+// Details ページのトップバー
+// Home ページへ戻るボタンを設置
+class NavBarDetails extends Component {
+  render() {
+    return (
+      <div>
+        <AppBar position="static" color="secondary">
+          <Toolbar>
+            <IconButton aria-label="Back">
+              <Link to="/" style={{ color: "white" }}><Icon>arrow_back</Icon></Link>
+            </IconButton>
+            <Typography variant="title" color="inherit">
+              GitHub Location Search - {this.props.page}
+            </Typography>
+          </Toolbar>
+        </AppBar>
+      </div>
+    );
+  }
+}
+
+export default NavBarDetails;
