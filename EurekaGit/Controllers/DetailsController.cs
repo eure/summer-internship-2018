2ba76a22cc71f34da907b23bad9c755ec4d2e0f0
selpo <planet_selpo@yahoo.co.jp>
2018-08-27 07:27:32 +0900

    課題の実装

---
+++ EurekaGit/Controllers/DetailsController.cs
@@ -0,0 +1,26 @@
+﻿using EurekaGit.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace EurekaGit.Controllers
+{
+    public class DetailsController : Controller
+    {
+        // GET: Details
+        public ActionResult Index(string owner, string name)
+        {
+			ViewBag.Owner = owner;
+			ViewBag.Name = name;
+			var k = new Tuple<string, string>(owner, name);
+			if (Downloader.IsOld) Downloader.GetRepositories();
+			foreach (var repo in Repository.Parsed)
+			{
+				if (repo.Owner == owner && repo.Name == name) ViewBag.Repository = repo;
+			}
+            return View();
+        }
+    }
+}
